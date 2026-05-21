1f3a31c298a8656b4519fa438fc47a9c5f8f8505
BrisaAimee <brisaaimeerodriguez@gmail.com>
2026-05-21 05:48:15 -0300

    nav

---
+++ src/app/componentes/Navbar.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className=" rounded-3xl py-4 px-30 flex items-center justify-between text-white bg-gradient-to-b from-zinc-950 to-zinc-600 shadow-[inset_0_4px_5px_rgba(255,255,255,0.2)]">
+      <div className="p-1 rounded-full bg-gradient-to-b from-zinc-300 to-black inline-block">
+        <h1 className="rounded-full font-[IBM.ttf] font-bold text-2xl p-2 bg-gradient-to-b from-zinc-950 to-zinc-600 shadow-[inset_0_4px_5px_rgba(255,255,255,0.2)]">
+          <Link href={"/"}>AeroPlay</Link>
+        </h1>
+      </div>
+      <div>
+        <ul className="flex flex-nowrap gap-4 font-[IBM.ttf] text-white">
+          <li className="px-1 py-2 rounded-full bg-gradient-to-b from-zinc-300 to-black inline-block">
+            <Link
+              href={"/"}
+              className="rounded-full font-[IBM.ttf] text-xl py-2 px-3 bg-gradient-to-b from-zinc-950 to-zinc-600 shadow-[inset_0_4px_5px_rgba(255,255,255,0.2)]"
+            >
+              Inicio
+            </Link>
+          </li>
+          <li className="px-1 py-2 rounded-full bg-gradient-to-b from-zinc-300 to-black inline-block">
+            <Link
+              href={"/"}
+              className="rounded-full font-[IBM.ttf] text-xl py-2 px-3 bg-gradient-to-b from-zinc-950 to-zinc-600 shadow-[inset_0_4px_5px_rgba(255,255,255,0.2)]"
+            >
+              Populares
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
